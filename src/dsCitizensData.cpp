1c699711b18d9eb8e91d66cf72b29e131fdb6dc2
JC Nesci <jc.nesci@gmail.com>
2014-07-11 16:00:25 -0700

    Added ofxGeoJson addon. No able to show the map of Boston yet. Translation problem.

--- src/dsCitizensData.cpp
+++ src/dsCitizensData.cpp
@@ -11,45 +11,80 @@
 dsCitizensData::dsCitizensData(string url){
   
   jsonUrl = url;
-  //  Get the data.
-  bool parsingSuccessful = jsonResults.open(url);
-  if (parsingSuccessful) {
-    cout  << "---------------- Successfully parsed JSON" << endl;
-    cout << jsonResults << endl;
-//		cout << jsonResults.getRawString() << endl;
-    /*
-    // now write pretty print
-    if(!jsonResults.save("example_output_pretty.json",true)) {
-      cout << "example_output_pretty.json written unsuccessfully." << endl;
-    } else {
-      cout << "example_output_pretty.json written successfully." << endl;
-    }
-    
-    // now write without pretty print
-    if(!jsonResults.save("example_output_fast.json",false)) {
-      cout << "example_output_pretty.json written unsuccessfully." << endl;
-    } else {
-      cout << "example_output_pretty.json written successfully." << endl;
-    }
-     */
-	} else {
-		cout  << "---------------- Failed to parse JSON" << endl;
-	}
- 
-  //  Store it in an object.
-  for(int i=0; i<jsonResults.size(); i++)
-	{
-    cout << "--- " << i << endl;
-    
-//    string title  = json["results"][i]["title"].asString();
-//		string author = json["results"][i]["byline"].asString();
-//		string date   = json["results"][i]["created_date"].asString();
-//		string text   = title + " - " + author + " (" + date + ")";
-//		ofDrawBitmapString(text, 20, i*24+40);
-	}
+  
+  fetchNewestJson();
+  fetchGeoJson();
   
 }
 
-dsCitizensData::~dsCitizensData(){
+dsCitizensData::~dsCitizensData(){}
+
+// Get the json data of requests from the Open311 system.
+void dsCitizensData::fetchNewestJson(){
   
+  bool parsingSuccessful = jsonResults.open(jsonUrl);
+  if (parsingSuccessful) {
+    //    cout  << "---------------- Successfully parsed JSON" << endl;
+    //		cout << jsonResults.getRawString() << endl;
+    
+    // Save to file : pretty print
+    if(!jsonResults.save("example_output_pretty.json",true)) {
+      //      cout << "example_output_pretty.json written unsuccessfully." << endl;
+    } else {
+      //      cout << "example_output_pretty.json written successfully." << endl;
+    }
+    // Save to file : raw
+    if(!jsonResults.save("example_output_fast.json",false)) {
+      //      cout << "example_output_pretty.json written unsuccessfully." << endl;
+    } else {
+      //      cout << "example_output_pretty.json written successfully." << endl;
+    }
+    
+	} else {
+    //		cout  << "---------------- Failed to parse JSON" << endl;
+	}
+  
+  //  Store it in an object.
+  for(int i=0; i<jsonResults.size(); i++)
+	{
+    event e;
+    e.id = i;
+    e.time = jsonResults[i]["updated_datetime"].asString();
+    e.status = jsonResults[i]["status"].asString();
+    e.lat = jsonResults[i]["lat"].asFloat();
+    e.lon = jsonResults[i]["long"].asFloat();
+    e.neighborhood = "unkown";
+    e.category = jsonResults[i]["service_name"].asString();
+    events.push_back(e);
+    
+    //    cout << "---- event["<< i <<"]" << endl;
+    //    cout << e.id << endl;
+    //    cout << e.time << endl;
+    //    cout << e.status << endl;
+    //    cout << e.lat << endl;
+    //    cout << e.lon << endl;
+    //    cout << e.neighborhood << endl;
+    //    cout << e.category << endl;
+	}
+
+}
+
+// Get geojson file of Boston neighborhoods.
+void dsCitizensData::fetchGeoJson(){
+  //  From the ofxGeoJSON example:
+  geojsonBoston.setMode(OFX_GEO_JSON_MERCATROE);
+  geojsonBoston.setScale(150000);
+  geojsonBoston.setTranslate(440, 320);     //TODO: find the proper translation to see the map in the canvas.
+  
+  if (geojsonBoston.load("boston_neighborhoods.geojson")) {
+    ofLog(OF_LOG_NOTICE, "Succeed to load geojson..");
+  } else {
+    ofLog(OF_LOG_NOTICE, "Failed to load geojson..");
+  };
+  
+}
+
+// Return the geojson.
+ofxGeoJSON dsCitizensData::getGeoJson(){
+  return geojsonBoston;
 }
